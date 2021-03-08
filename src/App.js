1175c980756f0e97b90b9962ae776b3907aa4a52
Mike Leonard <mtleonardjr@gmail.com>
2021-03-08 16:07:41 -0500

    laid out basic web design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className='header'>header</section>
+      <nav>
+        <ul>
+          <li className='logo'>logo</li>
+          <li className='invs'></li>
+          <li>1</li>
+          <li>1</li>
+        </ul>
+      </nav>
+      <main>
+        <section className='side-bar'>
+          Sidebar
+        </section>
+        <body>
+          <section className='banner'>banner</section>
+          <section className='body-grid'>body grid</section>
+          <section className='footer'>footer</section>
+        </body>
+      </main>
     </div>
   );
 }
