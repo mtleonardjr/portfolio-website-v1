409c6e75d6928172ab4b6b12858dec752e97337f
Mike Leonard <mtleonardjr@gmail.com>
2021-03-12 09:10:35 -0500

    refactored sidebar function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
+import React, { useState, useRef } from 'react'
 import './App.css';
 
 function App() {
 
-  const displaySB = () => {
-    var sb = document.getElementById("sidebar");
-    if (window.getComputedStyle(sb, null).getPropertyValue("display")=== "none") {
-      sb.style.display = "block";
-    } else {
-      sb.style.display = "none";
-    }
+  const [open, setOpen] = useState(false);
+  const [dropDown, setDropDown] = useState(false);
+
+  //Solution using hooks
+  const openSideBar = () => {
+    setOpen(!open);
   }
 
-  const displayDD = () => {
-    var dd = document.getElementById("dropdown-content");
-    if (window.getComputedStyle(dd, null).getPropertyValue("display")=== "none") {
-      dd.style.display = "flex";
-    } else {
-      dd.style.display = "none";
-    }
+  const openDropDown = () => {
+    setDropDown(!dropDown);
   }
+
+
+  // //Trad. JS and CSS solution
+  // const displaySB = () => {
+  //   var sb = document.getElementById("sidebar");
+  //   if (window.getComputedStyle(sb, null).getPropertyValue("display")=== "none") {
+  //     sb.style.display = "block";
+  //   } else {
+  //     sb.style.display = "none";
+  //   }
+  // }
+
+  // const displayDD = () => {
+  //   var dd = document.getElementById("dropdown-content");
+  //   if (window.getComputedStyle(dd, null).getPropertyValue("display")=== "none") {
+  //     dd.style.display = "flex";
+  //   } else {
+  //     dd.style.display = "none";
+  //   }
+  // }
   
   return (
     <div className="app">
       <section className='header'>header</section>
       <nav>
         <ul className='mobile-header'>
-          <button onClick={displaySB}>Sidebar btn</button>
+          <button onClick={openSideBar}>Sidebar btn</button>
           <h3>Michael's Portfolio</h3>
           <div className="dropdown">
-            <button className="dropbtn" onClick={displayDD}>Dropdown</button>
+            <button className="dropbtn" onClick={openDropDown}>Dropdown</button>
             <div className="dropdown-content" id="dropdown-content">
               <a href="#home">Link 1</a>
               <a href="#home">Link 2</a>
@@ -47,9 +62,7 @@ function App() {
         </ul>
       </nav>
       <section className='body'>
-        <section id='sidebar' className='sidebar'>
-          Sidebar
-        </section>
+        {open && <section id='sidebar' className='sidebar'>Sidebar</section>}
         <main>
           <section className='banner'>banner</section>
           <section className='main-grid'>main grid</section>
